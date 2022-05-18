4ad9b87d8551722fe609a72e521e6ea2b7114634
MichaelR139 <michaelradvinsky@gmail.com>
2022-05-17 21:11:44 -0700

    1st

---
+++ src/com/company/SolarSystem.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class SolarSystem {
+    String Sun;
+    String Moon;
+    String Planet;
+//add features to our sun
+public static void main(String[] args) {
+
+
+    FeatureSun fact = new FeatureSun();
+    fact.color = "green";
+    fact.radius="6";
+    fact.heat="9";
+}
+}
